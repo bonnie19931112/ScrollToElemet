728168f94f76379e41466d072a718a3281b54b8f
bonnie19931112 <bonnie111282@gmail.com>
2021-03-01 20:30:38 +0800

    完成

--- main.js
+++ main.js
@@ -3,18 +3,25 @@
 // 下載jQuery Code Snippets
 // jqeach element(如下)
 $("a").each(function (index, element) {
-    console.log("編號:"+index);
+    // console.log("編號:"+index);
 
     var target = $(this).attr("data-st-target");
     var duration = $(this).attr("data-st-duration");
+    var offsetTop = $(this).attr("data-st-offset");
 
-    console.log("目標" + target);
-    console.log("時間" + duration);
+    // console.log("目標" + target);
+    // console.log("時間" + duration);
+    // console.log("位移" + offsetTop);
 
     var offset = $("#" + target).offset();
     var top = offset.top;
 
-    console.log("上方" + top);
+    // 上方 扣除上方位移
+    // top = top - offsetTop; //原始寫法
+    // 如果 offsetTop 存在的話 才執行 剪髮運算
+    if (offsetTop) top -= offsetTop;
+
+    // console.log("上方" + top);
     
     $(this).click(function (e) { 
         e.preventDefault();
@@ -28,3 +35,28 @@ $("a").each(function (index, element) {
     });
 });
 
+// 使用者在滑鼠滾動時 停止動畫
+$("html").on("mousewheel", function () {
+    $("html").stop()
+});
+
+var arrow = $("#arrow");
+arrow.fadeOut();
+
+// 箭頭顯示與隱藏效果
+$(window).scroll(function () { 
+    var windowTop = $(this).scrollTop();
+    console.log("視窗前上方" + windowTop);
+
+    var arrowTop = arrow.attr("data-st-top");
+    var arrowTime = arrow.attr("data-st-time");
+    var arrowTimeInt = parseInt(arrowTime);   //將文字轉為數字
+
+    console.log("箭頭出現位置:" + arrowTop);
+    console.log("箭頭特效時間" + arrowTime);
+
+    // 如果 視窗位置 大於等於箭頭上方 就 淡入
+    if (windowTop >= arrowTop) arrow.stop().fadeIn(arrowTimeInt);
+    // 否則 就 淡出
+    else arrow.fadeOut(arrowTimeInt);
+});
